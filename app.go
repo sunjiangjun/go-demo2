f315fed720897d25fbd47aca0460c1bf8cb2a179
vike <vikesun@vikedeMacBook-Pro.local>
2025-06-18 12:42:46 +0800

    feat:

--- app.go
+++ app.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/sunjiangjun/xlog"
+import (
+	"github.com/sunjiangjun/go-demo/util"
+	"github.com/sunjiangjun/xlog"
+)
 
 func main() {
 
 	x := xlog.NewXLogger()
 	x.Info("hello world")
+
+	util.MaxInt(1, 2)
 }
